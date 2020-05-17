e826bf8687b6ddcc33cbeab96a3f3f4df33d33f6
song-eu <el9001@gmail.com>
2020-05-17 17:07:42 +0900

    :construction: Login/Signup

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,10 +1,6 @@
 // action을 dispatch하여 state를 reducer에 적힌대로 변경한다
 // initialState는 reducer와 연결되어 있음
 
-export const initialState = {
-  isLogin: false,
-  user: {},
-};
 
 // Action 이름
 // 매번 Action명을 문자열로 쓰는 것은 번거롭기대문에 상수사용
@@ -13,14 +9,24 @@ export const LOG_OUT = 'LOG_OUT';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 
+export const initialState = {
+  isLogin: false,
+  user: {},
+};
+
 // Action
 // 하나하나 action 오브젝트를 만드는 걸 수작업으로 하는 것을 피하기위해 함수사용
-export const loginAction = {
-  type: LOG_IN,
-  data: { nickname: '윤해은' },
+export const postLogin = (data) => {
+  console.log('postlogin', data)
+  return {
+    type: LOG_IN,
+    loginInfo: data
+  }
 };
-export const logoutAction = {
-  type: LOG_OUT,
+export const postlogout = () => {
+  return {
+    type: LOG_OUT
+  }
 };
 
 // Reducer
@@ -28,11 +34,19 @@ const reducer = (state = initialState, action) => {
   // reducers는 state와 action을 받아서 다음 새로운 state를 만들어낸다
   // action이 실행되면 reducer가 실행됨..!
   switch (action.type) {
-    case LOG_IN: {
+    case LOGIN_SUCCESS: {
+      console.log('login success -> userinfo', action)
       return {
         ...state,
         isLogin: true,
-        user: action.data,
+        user: action.userInfo,
+      };
+    }
+    case LOGIN_FAIL: {
+      console.log('login fail-> userinfo', action.data)
+      return {
+        ...state,
+        isLogin: false
       };
     }
     case LOG_OUT: {
--- src/sagas/user.js
+++ src/sagas/user.js
@@ -1,31 +1,59 @@
-import { call, all, fork, takeEvery, put } from 'redux-saga/effects';
-import { LOG_IN, LOG_OUT, LOGIN_SUCCESS, LOGIN_FAIL } from '../reducers/user';
+import {all, fork, call, put, takeEvery} from 'redux-saga/effects';
+import { LOG_IN, LOG_OUT, LOGIN_SUCCESS, LOGIN_FAIL } from '../reducers/user'
 
-function loginAPI() {
-  // 서버에 login 요청을 하는 부분
-  return true;
+const testaccount = {
+  email: 'test',
+  password: 'test',
+  user_id: 1,
+  height: 160,
+  weight: 50,
+  username: 'testuser'
 }
 
-function* login() {
+function loginAPI(data) {
+  // 서버에 login 요청을 하는 부분
+  console.log('login API called')
+  console.log('user info will get', data)
+  console.log('testInfo', testaccount)
+  if(data.email === testaccount.email) {
+    console.log('user email smae')
+    if(data.password === testaccount.password) {
+      console.log('user password same')
+      return testaccount
+    }
+  }
+  else{
+    return false;
+  }
+  // axios.post(ROOT_URL, data)
+}
+
+function* login(data) {
+  console.log('login called?')
   try {
-    yield call(loginAPI);
+    const userInfo = yield call(loginAPI, data.loginInfo);
     // call => 함수 동기적 호출
     // 서버에 로그인 요청 성공시 다음줄이 실행됨
-    yield put({
-      // put은 dispatch랑 동일
-      type: LOGIN_SUCCESS,
-    });
+    console.log('LOGIN userINFO???', userInfo)
+    if(userInfo){
+      yield put({
+        // put은 dispatch랑 동일
+        type: LOGIN_SUCCESS,
+        userInfo
+      });
+    }else{
+      yield put({
+        type:LOGIN_FAIL
+      })
+    }
   } catch (e) {
     // loginAPI 실패시 다음 put 실행
-    console.error(e);
-    yield put({
-      type: LOGIN_FAIL,
-    });
+    console.error(LOGIN_FAIL, e.msessage)
   }
 }
+
 function* watchLogin() {
-  // takeLatest => LOG_IN 액션이 dispatch 되길 기다렸다가 dispatch 될 때 login 제네레이터를 호출한다
-  yield takeEvery(LOG_IN, login);
+  yield takeEvery(LOG_IN, login)
 }
 
 export default function* userSaga() {
