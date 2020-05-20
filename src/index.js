ac9eb0b56e2ab9a9a9f46d391239ea0ddf3bc739
heaeun <yhehe228@gmail.com>
2020-05-20 19:07:46 +0900

    package-lock.json, package.json, app.js 수정

--- src/index.js
+++ src/index.js
@@ -8,13 +8,12 @@ import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas';
 import { Provider } from 'react-redux'; // 리액트 컴포넌트들의 리덕스 스테이트를 제공해줌
 import reducer from './reducers';
-import { BrowserRouter } from 'react-router-dom';
-
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const enhancer =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(sagaMiddleware)) // production 모드 일때는 리덕스데브 노출안함
     : compose(
         // production 모드가 아닐때 리덕스데브툴 노출
@@ -22,7 +21,7 @@ const enhancer =
         typeof window !== 'undefined' &&
           window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
           ? window.__REDUX_DEVTOOLS_EXTENSION__()
-          : f => f
+          : (f) => f
       );
 
 const store = createStore(
@@ -31,7 +30,7 @@ const store = createStore(
   // reducer ? action이 디스패치될때 스테이트를 어떻게 바꿔야할지 정의해두것
   // state + reducer => store
 );
-console.log("store.getState:", store.getState());
+console.log('store.getState:', store.getState());
 
 ReactDOM.render(
   // 자식 컴포넌트들이 리덕스 스테이트에 접근 가능해진다
@@ -40,7 +39,7 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </Provider>,
-  document.querySelector("#root")
+  document.querySelector('#root')
 );
 
 sagaMiddleware.run(rootSaga);
--- src/pages/app.js
+++ src/pages/app.js
@@ -1,64 +1,24 @@
-
 import React from 'react';
 import { connect, useDispatch, useSelector } from 'react-redux';
 import Main from './main';
-import Login from '../components/Login'
-import Signup from '../components/Signup'
-import { Switch, Route, Redirect } from 'react-router-dom';
+import Login from '../components/Login';
+import Signup from '../components/Signup';
+import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
+import {} from 'react-router';
 
 const App = () => {
-  const isLogin = useSelector(state => state.user.isLogin)
+  const isLogin = useSelector((state) => state.user.isLogin);
 
   return (
     <div>
+      <Route exact path="/" component={isLogin ? Main : Login} />
       <Switch>
-        <Route exact 
-              path = "/login"
-              component = {Login}
-        />
-        <Route exact 
-              path = "/signup" 
-              component = {Signup}
-        />
-        <Route exact 
-              path = "/main"
-              component = {Main}
-        />
-        <Route
-            path="/"
-            component = { isLogin ? Main:Login}
-          />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/main" component={Main} />
       </Switch>
-
-/*import React, { useState } from "react";
-import Main from "./main";
-import SearchInput from "../components/SearchInput";
-import ResultVideos from "../components/ResultVideos";
-import Modal from "react-modal";
-
-Modal.setAppElement(document.getElementById("root"));
-
-function App() {
-  //리액트 훅: useState()로 함수형 컴포넌트에서도 클래스형 컴포넌트의 기능을 사용할 수 있게 해줌.
-  //useState()는 클래스형 컴포넌트의 state의 선언과 관리를 짧고 직관적인 코드로 가능하게 해줌.
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  return (
-    <div>
-      <Main />
-      <button onClick={() => setModalIsOpen(true)}>영상 추가 +</button>
-      <>
-        <Modal isOpen={modalIsOpen}>
-          <SearchInput />
-          <ResultVideos />
-          <hr />
-          <button onClick={() => setModalIsOpen(false)}>선택완료</button>
-          <button onClick={() => setModalIsOpen(false)}>닫기</button>
-        </Modal>
-      </>
-*/
     </div>
   );
-}
+};
 
 export default App;
