a88fd6f4e5484d84a577c39557ebd9ff23d09097
yoharoo <nabongi@naver.com>
2020-05-27 20:45:28 +0900

    선택영상 디테일 정보 추가

--- src/reducers/video.js
+++ src/reducers/video.js
@@ -1,25 +1,31 @@
 //Store의 state는 오직 액션으로만 변경 가능
 const initialState = {
   videos: [],
-  selectedVideo: [],
+  selectedVideo: null,
+  selectedDetails: null
 };
 // action type(명령어). 액션은 하나의 객체
 export const LOAD_VIDEO = 'LOAD_VIDEO';
 export const SELECT_VIDEO = 'SELECT_VIDEO';
 export const LOAD_VIDEO_SUCCESS = 'LOAD_VIDEO_SUCCESS';
+export const LOAD_VIDEO_DETAILS = 'LOAD_VIDEO_DETAILS';
 
 // action creators(액션 메서드). 액션객체를 리턴함
-export const loadVideo = (searchTerm) => ({
+export const loadVideo = searchTerm => ({
   type: LOAD_VIDEO,
-  searchTerm,
+  searchTerm
 });
-export const loadVideoSuccess = (videos) => ({
+export const loadVideoSuccess = videos => ({
   type: LOAD_VIDEO_SUCCESS,
-  videos,
+  videos
 });
-export const selectVideo = (selected) => ({
+export const selectVideo = selected => ({
   type: SELECT_VIDEO,
-  selected,
+  selected
+});
+export const loadVideoDetails = selected => ({
+  type: LOAD_VIDEO_DETAILS,
+  selected
 });
 
 //액션을 받아서 새로운 상태를 반환함.
@@ -27,21 +33,28 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_VIDEO: {
       return {
-        ...state,
+        ...state
       };
     }
     case LOAD_VIDEO_SUCCESS: {
       console.log('action.data :', action.data);
       return {
         ...state,
-        videos: action.data,
+        videos: action.data
       };
     }
     case SELECT_VIDEO: {
       console.log('action.selected :', action.selected);
       return {
         ...state,
-        selectedVideo: action.selected,
+        selectedVideo: action.selected
+      };
+    }
+    case LOAD_VIDEO_DETAILS: {
+      console.log('선택 영상 디테일 :', action.data);
+      return {
+        ...state,
+        selectedDetails: action.data
       };
     }
     default: {
