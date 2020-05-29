2b60053ad23b46b77c6d64e2d8ad73060331e0d3
yoharoo <nabongi@naver.com>
2020-05-29 17:30:47 +0900

    로그인/사인인 스타일 추가

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,62 +3,65 @@ import { connect, useDispatch, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as loginAction from '../reducers/user';
 import { Link } from 'react-router-dom';
-import { LoginStyle } from '../style/LoginStyle';
+import { LoginStyle, GoSignup } from '../style/LoginStyle';
+import loginImg from '../../login_top.png';
 
-const Login = (props) => {
-	let [values, setValues] = useState({});
-	const dispatch = useDispatch();
-	const handleInputTextChange = (e) => {
-		const { name, value } = e.target;
-		setValues({ ...values, [name]: value });
-	};
+const Login = props => {
+  let [values, setValues] = useState({});
+  const dispatch = useDispatch();
+  const handleInputTextChange = e => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
 
-	const handleLogin = () => {
-		console.log('values will send', values);
-		dispatch(loginAction.postLogin(values));
-	};
-	const handleEnter = (e) => {
-		if (e.key === 'Enter') {
-			handleLogin();
-		}
-	};
+  const handleLogin = () => {
+    console.log('values will send', values);
+    dispatch(loginAction.postLogin(values));
+  };
+  const handleEnter = e => {
+    if (e.key === 'Enter') {
+      handleLogin();
+    }
+  };
 
-	return (
-		<div>
-			<LoginStyle>
-				<h1>
-					<span>Go-Fit!</span>
-				</h1>
-				<h3> Login </h3>
-				<div className="text">
-					Email
-					<div>
-						<input
-							name="email"
-							onChange={handleInputTextChange}
-							onKeyPress={handleEnter}
-						/>
-					</div>
-				</div>
-				<div className="text">
-					Password
-					<div>
-						<input
-							type="password"
-							name="password"
-							onChange={handleInputTextChange}
-							onKeyPress={handleEnter}
-						/>
-					</div>
-				</div>
-				<button onClick={handleLogin}>Login</button>
-				<Link to="/Signup">
-					<button>Sign up</button>
-				</Link>
-			</LoginStyle>
-		</div>
-	);
+  return (
+    <div>
+      <LoginStyle>
+        <img src={loginImg} width='318px' />
+        <h1>
+          <span>Go-Fit!</span>
+        </h1>
+        <div className='text'>
+          <div>
+            <input
+              name='email'
+              onChange={handleInputTextChange}
+              onKeyPress={handleEnter}
+              placeholder='이메일 주소'
+            />
+          </div>
+        </div>
+        <div className='text'>
+          <div>
+            <input
+              type='password'
+              name='password'
+              onChange={handleInputTextChange}
+              onKeyPress={handleEnter}
+              placeholder='비밀번호'
+            />
+          </div>
+        </div>
+        <button onClick={handleLogin}>로그인</button>
+        <Link to='/Signup'>
+          <GoSignup>
+            {/* <button>아직 계정이 없으신가요?</button> */}
+            아직 계정이 없으신가요?
+          </GoSignup>
+        </Link>
+      </LoginStyle>
+    </div>
+  );
 };
 
 export default Login;
-
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,71 +1,101 @@
 import React, { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
+import { SigninStyle, GoSignin, BodyInfo } from '../style/SignupStyle';
 import axios from 'axios';
 const ROOT_URL = 'http://localhost:7777/';
 
 const Signup = () => {
-	let [values, setValues] = useState({});
+  let [values, setValues] = useState({});
 
-	const handleInputTextChange = (e) => {
-		const { name, value } = e.target;
-		setValues({ ...values, [name]: value });
-	};
+  const handleInputTextChange = e => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
 
-	const handleSignup = () => {
-		console.log('gathering values', values);
-		//axios.post(POST_URL, values).then(res => redirect = true)
-		let signupURL = ROOT_URL + 'api/user/signup';
-		axios.post(signupURL, values).then((res) => {
-			console.log(res);
-			setValues({ redirect: true });
-		});
-	};
-	if (values.redirect) {
-		return <Redirect to="/" />;
-	}
-	return (
-		<div>
-			<h3> Signup </h3>
-			<div>
-				Email : <input name="email" onChange={handleInputTextChange} />{' '}
-			</div>
-			<div>
-				Password :
-				<input
-					type="password"
-					name="password"
-					onChange={handleInputTextChange}
-				/>
-			</div>
-			<div>
-				Password :
-				<input
-					type="password"
-					placeholder="check"
-					name="passwordChecker"
-					onChange={handleInputTextChange}
-				/>
-			</div>
-			<div>
-				Username : <input name="username" onChange={handleInputTextChange} />
-			</div>
-			<div>
-				Height :
-				<input name="height" onChange={handleInputTextChange} type="number" />
-				cm
-			</div>
-			<div>
-				Weight :
-				<input name="weight" onChange={handleInputTextChange} type="number" />
-				kg
-			</div>
+  const handleSignup = () => {
+    console.log('gathering values', values);
+    //axios.post(POST_URL, values).then(res => redirect = true)
+    let signupURL = ROOT_URL + 'api/user/signup';
+    axios.post(signupURL, values).then(res => {
+      console.log(res);
+      setValues({ redirect: true });
+    });
+  };
+  if (values.redirect) {
+    return <Redirect to='/' />;
+  }
+  return (
+    <div>
+      <SigninStyle>
+        <h3>
+          <span>회원가입</span>
+        </h3>
 
-			<button onClick={handleSignup}>OK</button>
-			<Link to="/">
-				<button>Cancel</button>
-			</Link>
-		</div>
-	);
+        <div>
+          <input
+            name='email'
+            onChange={handleInputTextChange}
+            placeholder='이메일 주소'
+          />{' '}
+        </div>
+        <div>
+          <input
+            type='password'
+            name='password'
+            onChange={handleInputTextChange}
+            placeholder='비밀번호'
+          />
+        </div>
+        <div>
+          <input
+            type='password'
+            placeholder='check'
+            name='passwordChecker'
+            onChange={handleInputTextChange}
+            placeholder='비밀번호 확인'
+          />
+        </div>
+        <div>
+          <input
+            name='username'
+            onChange={handleInputTextChange}
+            placeholder='사용자 이름'
+          />
+        </div>
+        <div>
+          <h6> * * *</h6>
+        </div>
+        <BodyInfo>
+          <div>
+            <input
+              name='height'
+              onChange={handleInputTextChange}
+              type='number'
+              placeholder='키'
+            />
+            cm
+          </div>
+
+          <div>
+            <input
+              name='weight'
+              onChange={handleInputTextChange}
+              type='number'
+              placeholder='현재 몸무게'
+            />
+            kg
+          </div>
+        </BodyInfo>
+        <button onClick={handleSignup}>가입</button>
+        <Link to='/'>
+          <GoSignin>
+            {/* <button>Cancel</button> */}
+            이미 계정이 있으신가요?
+          </GoSignin>
+        </Link>
+      </SigninStyle>
+    </div>
+  );
 };
 
 export default Signup;
