ffd8e11dc6b5cc8ec717dfa9964e87e0fde99ef5
yoharoo <nabongi@naver.com>
2020-05-30 17:10:09 +0900

    isVideoSelect 추가

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { selectVideo, loadVideoDetails } from '../reducers/video';
+import {
+	selectVideo,
+	loadVideoDetails,
+	IS_VIDEO_SELECT,
+	IS_VIDEO_UNSELECT
+} from '../reducers/video';
 
 const VideoItem = ({ video, thumbnail, title }) => {
-  const dispatch = useDispatch();
-  const handleSelect = () => {
-    //선택한 아이템을 selectedVideo로 담아줌
-    dispatch(selectVideo(video));
-    //선택한 아이템의 contentDetails를 호출하고 정보를 selectedDetails에 담아줌.
-    dispatch(loadVideoDetails(video));
-  };
+	const [isSelect, setSelect] = useState(false);
+	const dispatch = useDispatch();
 
-  return (
-    <div>
-      <input type="radio" name="select-item" onChange={handleSelect} />
-      <img src={thumbnail} />
-      <div>{title.slice(0, 24).concat('...')}</div>
-    </div>
-  );
+	const toggle = () => {
+		setSelect = !isSelect;
+		return setSelect === true
+			? dispatch({ type: IS_VIDEO_SELECT })
+			: dispatch({ type: IS_VIDEO_UNSELECT });
+	};
+	const handleChange = () => {
+		//선택한 아이템을 selectedVideo로 담아줌
+		dispatch(selectVideo(video));
+		//선택한 아이템의 contentDetails를 호출하고 정보를 selectedDetails에 담아줌.
+		dispatch(loadVideoDetails(video));
+		toggle();
+	};
+
+	return (
+		<div>
+			<input
+				type='radio'
+				name='select-item'
+				value={toggle}
+				onChange={handleChange}
+			/>
+			<img src={thumbnail} />
+			<div>{title.slice(0, 24).concat('...')}</div>
+		</div>
+	);
 };
 
 export default VideoItem;
--- src/reducers/video.js
+++ src/reducers/video.js
@@ -2,11 +2,12 @@
 import produce from 'immer';
 
 const initialState = {
-  videos: [],
-  selectedVideo: null,
-  selectedDetails: null,
-  videoList: [],
-  isEdit: false,
+	videos: [],
+	selectedVideo: null,
+	selectedDetails: null,
+	videoList: [],
+	isEdit: false,
+	isVideoSelect: false //액션 추가
 };
 // action type(명령어). 액션은 하나의 객체
 export const LOAD_VIDEO = 'LOAD_VIDEO';
@@ -20,91 +21,103 @@ export const ADD_VIDEO = 'ADD_VIDEO';
 export const EDIT_VIDEO = 'EDIT_VIDEO';
 export const START_EDIT_MODE = 'START_EDIT_MODE';
 export const END_EDIT_MODE = 'END_EDIT_MODE';
+export const IS_VIDEO_SELECT = 'IS_VIDEO_SELECT';
+export const IS_VIDEO_UNSELECT = 'IS_VIDEO_UNSELECT';
 
 // action creators(액션 메서드). 액션객체를 리턴함
-export const loadVideo = (searchTerm) => ({
-  type: LOAD_VIDEO,
-  searchTerm,
+export const loadVideo = searchTerm => ({
+	type: LOAD_VIDEO,
+	searchTerm
 });
 
-export const loadVideoSuccess = (videos) => ({
-  type: LOAD_VIDEO_SUCCESS,
-  videos,
+export const loadVideoSuccess = videos => ({
+	type: LOAD_VIDEO_SUCCESS,
+	videos
 });
 
-export const selectVideo = (selected) => ({
-  type: SELECT_VIDEO,
-  selected,
+export const selectVideo = selected => ({
+	type: SELECT_VIDEO,
+	selected
 });
 
-export const loadVideoDetails = (selected) => ({
-  type: LOAD_VIDEO_DETAILS,
-  selected,
+export const loadVideoDetails = selected => ({
+	type: LOAD_VIDEO_DETAILS,
+	selected
 });
 
 //액션을 받아서 새로운 상태를 반환함.
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case LOAD_VIDEO: {
-      return {
-        ...state,
-      };
-    }
-    case LOAD_VIDEO_SUCCESS: {
-      console.log('action.data :', action.data);
-      return {
-        ...state,
-        videos: action.data,
-      };
-    }
-    case SELECT_VIDEO: {
-      console.log('action.selected :', action.selected);
-      return {
-        ...state,
-        selectedVideo: action.selected,
-      };
-    }
-    case LOAD_VIDEO_DETAILS: {
-      console.log('선택 영상 디테일 :', action.data);
-      return {
-        ...state,
-        selectedDetails: action.data,
-      };
-    }
-    case REMOVE_SELECTED_VIDEO: {
-      return produce(state, (draft) => {
-        draft.selectedVideo = null;
-      });
-    }
-    case ADD_VIDEO: {
-      return produce(state, (draft) => {
-        draft.videoList = [...state.videoList, action.selectVideo];
-      });
-    }
-    case EDIT_VIDEO: {
-      return produce(state, (draft) => {
-        draft.videoList[action.editVideoIndex] = action.selectVideo;
-      });
-    }
-    case START_EDIT_MODE: {
-      return produce(state, (draft) => {
-        draft.isEdit = true;
-      });
-    }
-    case END_EDIT_MODE: {
-      return produce(state, (draft) => {
-        draft.isEdit = false;
-      });
-    }
-    case SUCCESS_VIDEO_LIST: {
-      return produce(state, (draft) => {
-        draft.videoList = action.videoList;
-      });
-    }
-    default: {
-      return state;
-    }
-  }
+	switch (action.type) {
+		case LOAD_VIDEO: {
+			return {
+				...state
+			};
+		}
+		case LOAD_VIDEO_SUCCESS: {
+			// console.log('action.data :', action.data);
+			return {
+				...state,
+				videos: action.data
+			};
+		}
+		case SELECT_VIDEO: {
+			// console.log('action.selected :', action.selected);
+			return {
+				...state,
+				selectedVideo: action.selected
+			};
+		}
+		case LOAD_VIDEO_DETAILS: {
+			// console.log('선택 영상 디테일 :', action.data);
+			return {
+				...state,
+				selectedDetails: action.data
+			};
+		}
+		case REMOVE_SELECTED_VIDEO: {
+			return produce(state, draft => {
+				draft.selectedVideo = null;
+			});
+		}
+		case ADD_VIDEO: {
+			return produce(state, draft => {
+				draft.videoList = [...state.videoList, action.selectVideo];
+			});
+		}
+		case EDIT_VIDEO: {
+			return produce(state, draft => {
+				draft.videoList[action.editVideoIndex] = action.selectVideo;
+			});
+		}
+		case START_EDIT_MODE: {
+			return produce(state, draft => {
+				draft.isEdit = true;
+			});
+		}
+		case END_EDIT_MODE: {
+			return produce(state, draft => {
+				draft.isEdit = false;
+			});
+		}
+		case SUCCESS_VIDEO_LIST: {
+			return produce(state, draft => {
+				draft.videoList = action.videoList;
+			});
+		}
+		case IS_VIDEO_SELECT: {
+			return produce(state, draft => {
+				draft.isVideoSelect = true;
+			});
+		}
+		case IS_VIDEO_UNSELECT: {
+			return produce(state, draft => {
+				draft.isVideoSelect = false;
+			});
+		}
+		default: {
+			return state;
+		}
+	}
 };
 
 export default reducer;
