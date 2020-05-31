28b8614d789ef92e2023f7790633459948b83cbb
heaeun <yhehe228@gmail.com>
2020-05-31 15:54:06 +0900

    비디오 검색 팝업에서 선택한 영상이 없을시에도 메인비디오 목록에 추가되는부분 해결

--- src/components/ResultVideos.js
+++ src/components/ResultVideos.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import VideoItem from './VideoItem';
+import { IS_VIDEO_SELECT } from '../reducers/video';
 
 const ResultVideos = () => {
-  const videos = useSelector(state => state.video.videos);
+  const videos = useSelector((state) => state.video.videos);
+  const dispatch = useDispatch();
   console.log('store.video.videos :', videos);
+
+  const onClickRadio = (e) => {
+    e.target.checked && dispatch({ type: IS_VIDEO_SELECT });
+  };
 
   return (
     <div>
-      {videos.map(video => {
+      {videos.map((video) => {
         return (
           <VideoItem
             key={video.etag}
             video={video}
             thumbnail={video.snippet.thumbnails.default.url}
             title={video.snippet.title}
+            onClickRadio={onClickRadio}
           />
         );
       })}
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,41 +1,43 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useEffect } from 'react-redux';
 import {
-	selectVideo,
-	loadVideoDetails,
-	IS_VIDEO_SELECT,
-	IS_VIDEO_UNSELECT
+  selectVideo,
+  loadVideoDetails,
+  IS_VIDEO_SELECT,
+  IS_VIDEO_UNSELECT,
 } from '../reducers/video';
 
-const VideoItem = ({ video, thumbnail, title }) => {
-	const [isSelect, setSelect] = useState(false);
-	const dispatch = useDispatch();
+const VideoItem = ({ video, thumbnail, title, onClickRadio }) => {
+  const [isSelect, setSelect] = useState(false);
+  const dispatch = useDispatch();
 
-	const toggle = () => {
-		setSelect = !isSelect;
-		return setSelect === true
-			? dispatch({ type: IS_VIDEO_SELECT })
-			: dispatch({ type: IS_VIDEO_UNSELECT });
-	};
-	const handleChange = () => {
-		//선택한 아이템을 selectedVideo로 담아줌
-		dispatch(selectVideo(video));
-		//선택한 아이템의 contentDetails를 호출하고 정보를 selectedDetails에 담아줌.
-		dispatch(loadVideoDetails(video));
-	};
+  const toggle = () => {
+    setSelect(!isSelect);
+  };
 
-	return (
-		<div>
-			<input
-				type='radio'
-				name='select-item'
-				value={toggle}
-				onChange={handleChange}
-			/>
-			<img src={thumbnail} />
-			<div>{title.slice(0, 24).concat('...')}</div>
-		</div>
-	);
+  const handleChange = () => {
+    toggle();
+    // isSelect
+    //   ? dispatch({ type: IS_VIDEO_SELECT })
+    //   : dispatch({ type: IS_VIDEO_UNSELECT });
+    //선택한 아이템을 selectedVideo로 담아줌
+    dispatch(selectVideo(video));
+    //선택한 아이템의 contentDetails를 호출하고 정보를 selectedDetails에 담아줌.
+    dispatch(loadVideoDetails(video));
+  };
+
+  return (
+    <div onClick={onClickRadio}>
+      <input
+        type="radio"
+        name="videoRadio"
+        checked={isSelect}
+        onChange={handleChange}
+      />
+      <img src={thumbnail} />
+      <div>{title.slice(0, 24).concat('...')}</div>
+    </div>
+  );
 };
 
 export default VideoItem;
